0792170d4d78ed02c2d6b79302d261f675a764d7
RobinCuy <cuyckensinnederland@gmail.com>
2026-09-16 10:34:00 +0200

    logos and images added along with styling

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styling.css">
+    <title>Curio</title>
+</head>
+<body>
+    <header>
+        <h1>De Curio opleiding!</h1>
+    </header>
+
+    <main>
+        <div class="textbox">
+            <h2>Hoe ziet het rooster van een eerstejaar eruit?</h2>
+            <p>Het rooster voor een eerstejaars student is erg soepel en niet overvallend. Voor mijn eerste blok had ik namelijk maar vier dagen school! Gemiddeld zal het rooster mind voelen als dat je op het middelbare had wat lijdt tot een erg gemakkelijke schooltijd! Je rooster zal voornamelijk bestaan uit PRA, WEB en NAT. Dit zijn de belangrijkste vakken van de opleiding. Ook zal je codelab in je rooster hebben staan, voor eerstejaars studenten hoef je niet alleen maar naartoe als je huiswerk niet af is, Dus hou het goed bij!</p>
+        </div>
+        <div class="center">
+            <div class="classes">
+                <div class="classheader">
+                    <h1>Welke klassen heb je in het eerste?</h1>
+                </div>
+                <div class="order">
+                    <div class="classbox">
+                        <h2>NAT</h2>
+                        <p>In NAT zul je aan de slag gaan met native codeer taalen, je zult met python werken om eenvoudige programmas te maken!</p>
+                    </div>
+                    <div class="classbox">
+                        <h2>WEB</h2>
+                        <p>In WEB zul je aan de slag gaan met HTML om eenvoudige websites te maken en beginnen je codeer skills te ontwikkelen!</p>
+                    </div>
+                    <div class="classbox">
+                        <h2>PRA</h2>
+                        <p>In PRA zul je samenwerken met andere mensen om te leren in een team omgeving projecten te maken.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="lower">
+            <img src="img/curio_building.jpeg" alt="Curio Building">
+            <div class="textbox">
+                <h2>Wat vind ik van de opleiding?</h2>
+                <p>Ik vind het een erg goede opleiding waar je niet alleen leert om te coderen en programmeren maar ook leert samenwerken met andere in een semi-werk omgeving. De vakken worden op een eenvoudige manier gegeven, waardoor het erg makkelijk is om de talen te leren. De docenten zijn ook erg aardig en je zult ze altijd kunnen bereiken als je hulp nodig hebt. De stof is verder ook online beschikbaar dus als je iets heb gemist kun je altijd terugkijken.</p>
+            </div>
+        </div>
+    </main>
+        
+    <footer>
+        <img src="img/curio-03-geel-logo-rgb.png" alt="Curio Logo" width="100px">
+        <p>Curio, Terheijdenseweg 350</p>
+    </footer>
+</body>
+</html>
